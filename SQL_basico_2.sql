66240a9ad2f0976732eef22254594b3a2357f0ea
Pedro Pizetta <123319139+PedroPizetta@users.noreply.github.com>
2023-02-06 21:34:19 -0300

    Add files via upload

---
+++ SQL_basico_2.sql
@@ -0,0 +1,9 @@
+SELECT TOP 10 * FROM olist_customers_dataset
+SELECT TOP 10 * FROM olist_geolocation_dataset
+SELECT TOP 10 * FROM olist_order_items_dataset
+SELECT TOP 10 * FROM olist_order_payments_dataset
+SELECT TOP 10 * FROM olist_order_reviews_dataset
+SELECT TOP 10 * FROM olist_orders_dataset
+SELECT TOP 10 * FROM olist_products_dataset
+SELECT TOP 10 * FROM olist_sellers_dataset
+SELECT TOP 10 * FROM product_category_name_translation

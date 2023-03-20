89f2956916a4b83fef1e8b6a585f71e1c8f369e2
Pedro Pizetta <123319139+PedroPizetta@users.noreply.github.com>
2023-03-20 15:58:48 -0300

    Update SQL_basico_2.sql

--- SQL_basico_2.sql
+++ SQL_basico_2.sql
@@ -1,9 +1,24 @@
 SELECT TOP 10 * FROM olist_customers_dataset
-SELECT TOP 10 * FROM olist_geolocation_dataset
-SELECT TOP 10 * FROM olist_order_items_dataset
-SELECT TOP 10 * FROM olist_order_payments_dataset
-SELECT TOP 10 * FROM olist_order_reviews_dataset
-SELECT TOP 10 * FROM olist_orders_dataset
-SELECT TOP 10 * FROM olist_products_dataset
-SELECT TOP 10 * FROM olist_sellers_dataset
-SELECT TOP 10 * FROM product_category_name_translation
+
+SELECT * FROM Customers;
+
+SELECT COUNT (First_name)
+From Customers 
+Order by age asc;
+
+SELECT C.First_name as Nome, C.Last_name AS [Sobrenome], O.item AS [Items], O. Amount AS VALOR
+FROM Customers AS C
+INNER JOIN Orders AS O ON C.customer_id = O.customer_id;
+
+SELECT first_name,age, country
+FROM Customers
+Where country = "USA"
+Group by age;
+
+CREATE VIEW VW_PEDIDOS_POR_CLIENTE
+AS
+SELECT C.First_name as Nome, C.Last_name AS [Sobrenome], O.item AS [Items], O. Amount AS VALOR
+FROM Customers AS C
+INNER JOIN Orders AS O ON C.customer_id = O.customer_id;
+
+
